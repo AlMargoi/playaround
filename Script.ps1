99533456ef03e51af9dc48e50013468fb3c769cf
Alexandru Margoi <alexandru.margoi@outlook.com>
2020-10-21 17:29:48 +0200

    Cristina update

--- Script.ps1
+++ Script.ps1
@@ -1,5 +1,4 @@
 $x = Get-Time 
 Write-Output "$x"
 Write-Output "Time should have been displayed. It is quite late for work related stuff."
-#WebhookURL: https://049d0085-7f07-4433-99b5-21ddbcfc6ebe.webhook.we.azure-automation.net/webhooks?token=UbXId83t%2fe883%2f7df43wWPrckvryV6RAGyidPdxyOWI%3d
-# Additional useless comment here
+Write-Output "Cristina = cute"
