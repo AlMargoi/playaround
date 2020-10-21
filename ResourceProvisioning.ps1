c9f6fcd15b8337d02882bb9d101515c21ab52066
Alexandru Margoi <alexandru.margoi@outlook.com>
2020-10-21 17:21:22 +0200

    V1

--- ResourceProvisioning.ps1
+++ ResourceProvisioning.ps1
@@ -10,7 +10,7 @@ param(
 )
 
 #region check/create Resource Group
-Write-Output "> `nStart: Check/Create Resource Group: $ResourceGroupName...    "
+Write-Output "`n > Start: Check/Create Resource Group: $ResourceGroupName...    "
 if(Get-AzResourceGroup -Name $ResourceGroupName -ErrorAction SilentlyContinue){
     Write-Output "ResourceGroup $ResourceGroupName exists. Skipping creation"
 }
@@ -28,7 +28,7 @@ else{
 #endregion check/create Resource Group
  
 #region check/create Automation Account
-Write-Output "> `nStart Check/Create Automation Account $AutomationAccount..."
+Write-Output "`n > Start Check/Create Automation Account $AutomationAccount..."
 if(Get-AzAutomationAccount -Name $AutomationAccountName -ResourceGroupName $ResourceGroupName -ErrorAction SilentlyContinue){
     Write-Output "Automation Account $AutomationAccountName exists. Skipping creation"
 }
@@ -46,7 +46,7 @@ else{
 #endregion check/create Automation Account
 
 #region import Runbook
-Write-Output "> `nStart check/create $RunbookName runbook."
+Write-Output "`n > Start check/create $RunbookName runbook."
 if($Runbook = Get-AzAutomationRunbook -Name $RunbookName -ResourceGroupName $ResourceGroupName -AutomationAccountName $AutomationAccountName -ErrorAction SilentlyContinue){
     Write-Output "Automation Runbook $RunbookName exists. Creation time: $($Runbook.CreationTime). Last modified time: $($Runbook.LastModifiedTime)"
     Write-Output "Will attept to overwrite with newest version from the Devops Git Repo."
@@ -54,10 +54,10 @@ if($Runbook = Get-AzAutomationRunbook -Name $RunbookName -ResourceGroupName $Res
     Write-Output "Automation Runbook $RunbookName does not exist. Attempting import."
 }
 
-Write-Output "> `nStart importing the [Repository\$($RunbookPath)] script into the Runbook."
+Write-Output "`n > Start importing the [Repository\$($RunbookPath)] script into the Runbook."
 try{
     Import-AzAutomationRunbook -Name $RunbookName -Path $RunbookPath -ResourceGroupName $ResourceGroupName `
-         -AutomationAccountName $AutomationAccountName -Type $RunbookType  -Force -ErrorAction STOP
+         -AutomationAccountName $AutomationAccountName -Type $RunbookType  -Force -ErrorAction STOP | Out-Null
     Write-Output "Automation Runbook $RunbookName successfully imported."
 }catch{
     $Exception = $_.Exception
@@ -67,9 +67,9 @@ try{
 #endregion import Runbook
 
 #region publish the automation runbook
-Write-Output "> `nStart publishing the $RunbookName runbook."
+Write-Output "`n > Start publishing the $RunbookName runbook."
 try{
-    Publish-AzAutomationRunbook -AutomationAccountName $AutomationAccountName -Name $RunbookName -ResourceGroupName $ResourceGroupName -ErrorAction STOP
+    Publish-AzAutomationRunbook -AutomationAccountName $AutomationAccountName -Name $RunbookName -ResourceGroupName $ResourceGroupName -ErrorAction STOP | Out-Null
     Write-Output "Succesfully published the automation runbook."
 }catch{
     $Exception = $_.Exception
@@ -79,7 +79,7 @@ try{
 #endregion publish the automation runbook
 
 #region check/create webhook
-Write-Output "> `nStart check/create $WebhookName webook."
+Write-Output "`n > Start check/create $WebhookName webook."
 $Webhook = Get-AzAutomationWebhook -RunbookName $RunbookName -AutomationAccountName $AutomationAccountName `
              -ResourceGroupName $ResourceGroupName  -ErrorAction SilentlyContinue | ? {$_.Name -eq $WebhookName}
 if($Webhook){
@@ -88,7 +88,7 @@ if($Webhook){
     try{
         Write-Output "Attempting to create $WebhookName webhook." 
         $Webhook = New-AzAutomationWebhook -Name $WebhookName -IsEnabled $True -ExpiryTime (Get-Date).AddYears(1) -RunbookName $RunbookName -ResourceGroup $ResourceGroupName -AutomationAccountName $AutomationAccountName -Force
-        Write-Output "Webhook $Webhook successfully created. WebhookURI: $($Webhook.WebhookURI)" 
+        Write-Output "Webhook $($Webhook.Name) successfully created. WebhookURI: $($Webhook.WebhookURI)" 
     }catch{
         $Exception = $_.Exception
         Write-Output "ERROR: cannot create webhook: $($Exception.GetType()) - $($Exception.Message)"
